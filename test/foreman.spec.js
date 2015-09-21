6c8ccdd643882c00b44d8026241586fb9fd0fb14
Marko Lalic <marko.lalic@gmail.com>
2015-09-21 18:21:11 +0200

    Import range module

--- test/foreman.spec.js
+++ test/foreman.spec.js
@@ -1,6 +1,8 @@
 import { assert } from 'chai';
 import itertools from '../src/itertools';
 
+import BaseRange from '../src/range';
+
 describe('Foreman', () => {
   describe('Brick', () => {
     it("works", () => {
@@ -19,6 +21,42 @@ describe('Foreman', () => {
       ];
 
       assert.deepEqual(res, [3, 4, 5]);
+    });
+  });
+
+  describe('range', () => {
+    it('range is re-entrant', () => {
+      const range = BaseRange.fromForLike(0, x => x + 1, x => x == 10);
+      const first = itertools.collect(range);
+      const second = itertools.collect(range);
+      const expected = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+      assert.deepEqual(first, expected);
+      assert.deepEqual(second, expected);
+    });
+
+    it('range from Array', () => {
+      const list = [1, 2, 3, 4];
+      const range = BaseRange.fromIterable(list);
+
+      const first = itertools.collect(range);
+      const second = itertools.collect(range);
+
+      assert.deepEqual(first, list);
+      assert.deepEqual(second, list);
+    });
+
+    it('range from iterator', () => {
+      const list = [1, 2, 3, 4];
+      const range = BaseRange.fromIterable(list[Symbol.iterator]());
+
+      const first = itertools.collect(range);
+      const second = itertools.collect(range);
+
+      assert.deepEqual(first, list);
+      // Once the first iteration is complete, the iterator is exhausted, so it is expected
+      // that the second one is empty...
+      assert.deepEqual(second, []);
     });
   });
 });
