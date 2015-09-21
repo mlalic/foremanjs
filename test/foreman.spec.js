c83257ae0fe174a3145b2e03affba1d4fdd9e0e0
Marko Lalic <marko.lalic@gmail.com>
2015-09-21 18:21:11 +0200

    Import itertools module

--- test/foreman.spec.js
+++ test/foreman.spec.js
@@ -1,9 +1,24 @@
 import { assert } from 'chai';
+import itertools from '../src/itertools';
 
 describe('Foreman', () => {
   describe('Brick', () => {
     it("works", () => {
       assert.equal(1, 1);
+    });
+  });
+
+  describe('itertools', () => {
+    it('`map` works', () => {
+      let map1 = itertools.map(x => x + 1, [1, 2, 3]);
+      let map2 = itertools.map(x => x + 1, map1);
+
+      const res = [
+        for (x of map2)
+        x
+      ];
+
+      assert.deepEqual(res, [3, 4, 5]);
     });
   });
 });
